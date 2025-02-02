7f25599905ec5cb390c674f16694b56de07a6e4e
Saurabh <saurabh.shende25@gmail.com>
2025-02-02 15:58:01 +0530

    separating successCallBack and errorCallBack for readability

--- First Mock API/exampleController.js
+++ First Mock API/exampleController.js
@@ -3,13 +3,17 @@ app.controller('exampleController', function ($scope,$http) {
     debugger;
     $scope.data = {};
 
-    $http.get('https://jsonplaceholder.typicode.com/todos/1')   //third party mock api
-    .then(function(response){
-        console.log(response);
+    var successCallBack = function(response){
+        console.log("success : ",response);
         $scope.data=response.data;
+    }
 
-    },function(error){
-        console.log(error);
-    })
+    var errorCallBack = function(error){
+        console.log("error : ",error);
+        alert('Error!!')
+    }
+
+    $http.get('https://jsonplaceholder.typicode.com/todos/1')   //third party mock api
+    .then(successCallBack , errorCallBack);
 
 })
